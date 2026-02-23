5cfd2fdad0d6e6759dbd692ae0d8be957e784a8a
Prajapati1324 <prajapatirahul2004@gmail.com>
2026-02-23 10:56:52 +0530

    Nodemailer Error Checked

--- server.js
+++ server.js
@@ -221,13 +221,24 @@ app.get('/installs', (req, res) => {
 ---------------------------------- */
 async function sendEmail(ownerEmail, instanceId) {
 
+  // const transporter = nodemailer.createTransport({
+  //   service: 'gmail',
+  //   auth: {
+  //     user: process.env.EMAIL_USER,
+  //     pass: process.env.EMAIL_PASS
+  //   }
+  // })
+
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS
-    }
-  })
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
+  family: 4, // Force IPv4
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
+});
 
   await transporter.sendMail({
     from: process.env.EMAIL_USER,
